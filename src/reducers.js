e0b2118cd74745c8554dd8c46029dcd949491c81
byronskout <vyskoutaris@hotmail.com>
2019-08-29 11:54:04 +0100

    Reducers.js functions added

--- src/reducers.js
+++ src/reducers.js
@@ -10,5 +10,38 @@ function selectedSubreddit(state = 'reactjs', action) {
     }
   }
 
-  
+  function posts(
+    state = {
+      isFetching: false,
+      didInvalidate: false,
+      items: []
+    },
+    action
+  ) {
+    switch (action.type) {
+      case INVALIDATE_SUBREDDIT:
+        return Object.assign({}, state, {
+          didInvalidate: true
+        })
+      case REQUEST_POSTS:
+        return Object.assign({}, state, {
+          isFetching: true,
+          didInvalidate: false
+        })
+      case RECEIVE_POSTS:
+        return Object.assign({}, state, {
+          isFetching: false,
+          didInvalidate: false,
+          items: action.posts,
+          lastUpdated: action.receivedAt
+        })
+      default:
+        return state
+    }
+  }
+
+  const rootReducer = combineReducers({
+    selectedSubreddit
+  })
+
 export default rootReducer
